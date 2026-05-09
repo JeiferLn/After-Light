dadc55148579b668cc0b73cceb8cedb39b9d4d8d
Mark-Barrera <haseo001@gmail.com>
2026-05-09 13:37:17 -0500

    feat: update ItemData to include unique itemID generation, consumable values, and stack size for enhanced inventory management

--- Assets/Scripts/Scriptables/SOScripts/ItemData.cs
+++ Assets/Scripts/Scriptables/SOScripts/ItemData.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class ItemData : ScriptableObject
 {
-    public string itemID;
+    public string itemID = System.Guid.NewGuid().ToString();
     public string itemName;
     public Sprite icon;
     public ItemType itemType;
 
+    [ShowIf("itemType", ItemType.Consumable)]
+    [Header("Consumible Values")]
+    public int recoveryVal = 10;
+
     [Header("Stack")]
     public bool stackable = true;
+    public int stackSize = 1;
     public int maxStack = 99;
 }
